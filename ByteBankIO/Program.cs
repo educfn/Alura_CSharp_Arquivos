cd8052408cb195478716fb0d572b8e7fdd74ebf5
Eduardo C F Neto <eduardo@verion.ind.br>
2024-12-17 17:05:37 -0300

    Fazer a seção 1: Criando um FileStream.

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
+        var enderecoDoArquivo = "contas.txt";
+        var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
+
+        var buffer = new byte[1024]; //1KB
+
+        // public override int Read(byte[] array, int offset, int count);
 
         Console.ReadLine();
     }
