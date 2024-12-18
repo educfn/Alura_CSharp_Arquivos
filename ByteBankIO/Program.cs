59e444d911c5b5d9872eb3fdd3bf09c44af2aa7a
Eduardo C F Neto <eduardo@verion.ind.br>
2024-12-18 10:51:45 -0300

    Fazer a seção 1: Método especializado.

--- ByteBankIO/Program.cs
+++ ByteBankIO/Program.cs
@@ -5,12 +5,36 @@ class Program
     static void Main(string[] args)
     {
         var enderecoDoArquivo = "contas.txt";
+        var numeroDeBytesLidos = -1;
+
         var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open);
 
         var buffer = new byte[1024]; //1KB
 
+        while (numeroDeBytesLidos != 0)
+        {
+            numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+            EscreverBuffer(buffer);
+        }
+
+        fluxoDoArquivo.Read(buffer, 0, 1024);
+
+        // Devoluções:
+        // 0 número total de bytes lidos do buffer. Isso poderá ser menor que o número de
+        // bytes solicitado se esse número de bytes não estiver disponível no momento, ou
+        //zero, se o final do fluxo for atingido
+
         // public override int Read(byte[] array, int offset, int count);
 
         Console.ReadLine();
+    }
+
+    static void EscreverBuffer(byte[] buffer)
+    {
+        foreach(var meuByte in buffer)
+        {
+            Console.Write(meuByte);
+            Console.Write(" ");
+        }
     }
 }
